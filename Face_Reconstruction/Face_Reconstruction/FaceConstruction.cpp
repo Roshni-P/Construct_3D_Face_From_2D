42bcb541b6847d81f9ee77f7ad5d5584d91db5a5
Roshni-P <roshni.praful@gmail.com>
2025-07-23 22:28:10 +0530

    Added code that generates face 3D mesh as obj file.

--- Face_Reconstruction/Face_Reconstruction/FaceConstruction.cpp
+++ Face_Reconstruction/Face_Reconstruction/FaceConstruction.cpp
@@ -27,10 +27,12 @@
 #include "eos/fitting/orthographic_camera_estimation_linear.hpp"
 #include "eos/render/texture_extraction.hpp"
 
-
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/imgcodecs/imgcodecs.hpp"
+
+#include "glut.h"
+
 using namespace cv;
 using namespace cv::face;
 
@@ -112,7 +114,7 @@ string FaceConstruction::OpenImageFile(const char* filter, HWND owner)
 	//Load Surrey Face Model
 	Ptr<Facemark> facemark = FacemarkLBF::create();
 	// Load landmark detector
-	facemark->loadModel("D:\\Project\\install\\opencv\\lbfmodel.yaml");
+	facemark->loadModel("..\\Data\\lbfmodel.yaml");
 	cout << "Loaded model" << endl;
 
 	vector<Rect> faces;
@@ -141,12 +143,13 @@ string FaceConstruction::OpenImageFile(const char* filter, HWND owner)
 		for (unsigned long i = 0; i < faces.size(); i++) 
 		{
 			for (unsigned long k = 0; k < shapes[i].size(); k++)
-				cv::circle(img, shapes[i][k], 5, cv::Scalar(0, 0, 255), FILLED);
+				cv::circle(img, shapes[i][k], 1, cv::Scalar(0, 0, 255), FILLED);
 		}
+		imshow("Detected_shape", img);
 	}
 
 	//Fit Face model
-	string modelfile = "sfm_shape_3448.bin";
+	string modelfile = "..\\Data\\sfm_shape_3448.bin";
 	morphablemodel::MorphableModel morphable_model;
 	try
 	{
@@ -158,14 +161,78 @@ string FaceConstruction::OpenImageFile(const char* filter, HWND owner)
 		return EXIT_FAILURE;
 	}
 
+	string mappingsfile = "..\\Data\\ibug_to_sfm.txt";
+	core::LandmarkMapper landmark_mapper;
+	try
+	{
+		landmark_mapper = core::LandmarkMapper(mappingsfile);
+	}
+	catch (const std::exception& e)
+	{
+		cout << "Error loading the landmark mappings: " << e.what() << endl;
+		return EXIT_FAILURE;
+	}
+
 	//Render Face
 	// These will be the final 2D and 3D points used for the fitting:
 	vector<Vector4f> model_points; // the points in the 3D shape model
 	vector<int> vertex_indices;    // their vertex indices
 	vector<Vector2f> image_points; // the corresponding 2D landmark points
+	//vector<Point2f> cv_img_points;
+	
+	// Sub-select all the landmarks which we have a mapping for (i.e. that are defined in the 3DMM):
+	for (unsigned long i = 0; i < faces.size(); i++)
+	{
+		for (int k = 0; k < shapes[i].size(); ++k)
+		{
+			const auto converted_name = landmark_mapper.convert(std::to_string(k+1));
+			if (!converted_name)
+			{ // no mapping defined for the current landmark
+				continue;
+			}
+			const int vertex_idx = std::stoi(converted_name.value());
+			const auto vertex = morphable_model.get_shape_model().get_mean_at_point(vertex_idx);
+			model_points.emplace_back(vertex.homogeneous());
+			vertex_indices.emplace_back(vertex_idx);
+			image_points.emplace_back(shapes[i][k].x, shapes[i][k].y);
+		}
+	}
 
+	// Estimate the camera (pose) from the 2D - 3D point correspondences
+	fitting::ScaledOrthoProjectionParameters pose =
+		fitting::estimate_orthographic_projection_linear(image_points, model_points, true, img.rows);
+	fitting::RenderingParameters rendering_params(pose, img.cols, img.rows);
 
-	//Light & Pose
+	// The 3D head pose can be recovered as follows - the function returns an Eigen::Vector3f with yaw, pitch,
+	// and roll angles:
+	const float yaw_angle = rendering_params.get_yaw_pitch_roll()[0];
 
-	return 0;
+	// Estimate the shape coefficients by fitting the shape to the landmarks:
+	const Eigen::Matrix<float, 3, 4> affine_from_ortho =
+		fitting::get_3x4_affine_camera_matrix(rendering_params, img.cols, img.rows);
+	const vector<float> fitted_coeffs = fitting::fit_shape_to_landmarks_linear(
+		morphable_model.get_shape_model(), affine_from_ortho, image_points, vertex_indices);
+
+	// Obtain the full mesh with the estimated coefficients:
+	const core::Mesh mesh = morphable_model.draw_sample(fitted_coeffs, vector<float>());
+
+	// Extract the texture from the image using given mesh and camera parameters:
+	const core::Image4u texturemap =
+		render::extract_texture(mesh, rendering_params.get_modelview(), rendering_params.get_projection(),
+			render::ProjectionType::Orthographic, core::from_mat_with_alpha(img));
+
+	// Save the mesh as textured obj:
+	fs::path outputfile = /*outputbasename +*/ "out.obj";
+	core::write_textured_obj(mesh, outputfile.string());
+
+	// And save the texture map:
+	outputfile.replace_extension(".texture.png");
+	cv::imwrite(outputfile.string(), core::to_mat(texturemap));
+
+	cout << "Finished fitting and wrote result mesh and texture to files with basename "
+		<< outputfile.stem().stem() << "." << endl;
+
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+
+	return EXIT_SUCCESS;
 }
